2be2a50f08eaf62429d737cc5daf5dced45062fd
designtechti <mrc_490@hotmail.com>
2020-06-09 15:57:16 -0300

    Adicionad a versão 2.0

---
+++ v2.0/index.php
@@ -0,0 +1,221 @@
+<?php 
+	include ('assets/includes/conexao.php');
+?>
+<!DOCTYPE HTML>
+<!--
+	Design Tech TI by DESIGN TECH TI | Web Development
+	designtechti.com.br @designtechti
+	Design & Tecnologia em Um só Lugar!
+-->
+<html lang="pt-br">
+	<head>
+		<title>Design Tech TI | Desenvolvimento Web</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="shortcut icon" href="images/favicon.png" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<div class="logo">
+					<a href="index.php">
+						<img src="images/logo.png">
+						<span>DESIGN TECH TI</span>
+					</a>
+				</div>
+				<a href="#menu">Menu</a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.php">Início</a></li>
+					<li><a href="sobre.php">Sobre</a></li>
+					<li><a href="portifolio.php">Portifólio</a></li>
+					<li><a href="blog.php">Blog</a></li>
+				</ul>
+			</nav>
+
+		<!-- Banner -->
+			<section class="banner full">
+				<article>
+					<img src="images/slide01.jpg" alt="" />
+					<div class="inner">
+						<header>
+							<p>Desenvolvimento de websites responsivos, Programação Front & Back-end.</p>
+							<h2>Desenvolvimento Web</h2>
+						</header>
+					</div>
+				</article>
+				<article>
+					<img src="images/slide02.jpg" alt="" />
+					<div class="inner">
+						<header>
+							<p>Interações com a Internet e dispositivos eletrônicos.</p>
+							<h2>Internet das Coisas</h2>
+						</header>
+					</div>
+				</article>
+				<article>
+					<img src="images/slide03.jpg"  alt="" />
+					<div class="inner">
+						<header>
+							<p>Manutenção & Suporte de TI em SO's Windows e Linux</p>
+							<h2>Soluções em TI</h2>
+						</header>
+					</div>
+				</article>
+				<article>
+					<img src="images/slide04.jpg"  alt="" />
+					<div class="inner">
+						<header>
+							<p>Notícias & curiosidades sobre tecnologia</p>
+							<h2>Blog</h2>
+						</header>
+					</div>
+				</article>
+				<article>
+					<img src="images/slide05.jpg"  alt="" />
+					<div class="inner">
+						<header>
+							<p>Edição de Audio e Vídeo</p>
+							<h2>Edição Audio-Visual</h2>
+						</header>
+					</div>
+				</article>
+			</section>
+
+		<!-- One -->
+		
+			<section id="one" class="wrapper style2">
+				<div class="inner">
+					<div class="grid-style">
+
+						<div>
+							<div class="box">
+								<div class="image fit zoom">
+									<img src="images/sublime.jpg" alt="" class="img-responsive" />
+								</div>
+								<div class="content">
+									<header class="align-center">
+										<p>Notícias & curiosidades sobre tecnologia</p>
+										<h2>Sublime Text: O queridinho dos programadores</h2>
+									</header>
+									<p>O que faz um profissional importante é seu conhecimento sobre as diversas áreas em que ele atua. Isso serve para qualquer profissional, do lixeiro ao alto executivo. Mas um outro fator importante a ser considerado são as ferramentas que o profissional utiliza. Já imaginou o quanto sofre um borracheiro sem um macaco? Profissional competente com as ferramentas certas é o primeiro passo para qualquer negócio ter sucesso.</p>
+									<footer class="align-center">
+										<a href="#" class="button alt">Leia Mais</a>
+									</footer>
+								</div>
+							</div>
+						</div>
+
+						<div>
+							<div class="box">
+								<div class="image fit zoom">
+									<img src="images/wireframes.jpg" alt="" class="img-responsive" />
+								</div>
+								<div class="content">
+									<header class="align-center">
+										<p>Notícias & curiosidades sobre tecnologia</p>
+										<h2>UX e acessibilidade ao navegar na web</h2>
+									</header>
+									<p>Saiba como um deficiente visual consegue navegar nas suas redes sociais, sites de notícias, aplicativos e programas de computador e como desenvolvedores de softwares e web designers ajudam a melhorar o acesso a tecnologia. </p>
+									<footer class="align-center">
+										<a href="#" class="button alt">Leia Mais</a>
+									</footer>
+								</div>
+							</div>
+						</div>
+
+						<div>
+							<div class="box">
+								<div class="image fit zoom">
+									<img src="images/iot.jpg" alt="" class="img-responsive" />
+								</div>
+								<div class="content">
+									<header class="align-center">
+										<p>Notícias & curiosidades sobre tecnologia</p>
+										<h2>IOT - O futuro da humanidade</h2>
+									</header>
+									<p>Entenda como a chamada Internet Das Coisas está mudando o conceito de tecnologia e como isto pode facilitar a nossa vida, trazendo mais conforto e modernidade no cotidiano. </p>
+									<footer class="align-center">
+										<a href="#" class="button alt">Leia Mais</a>
+									</footer>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</section>
+		</div>	
+
+		<!-- Two -->		
+			<section id="two" class="wrapper style3">
+				<div class="inner">
+					<header class="align-center">
+						<p>Design & Tecnologia em Um Só Lugar!</p>
+						<h2>Design Tech TI</h2>
+					</header>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three" class="wrapper style2">
+				<div class="inner">
+					<header class="align-center">
+						<p class="special">Alguns de nossos projetos</p>
+						<h2>Portifólio Web Design</h2>
+					</header>
+					<div class="gallery">
+						<div class="zoom">
+							<div class="image fit">
+								<a href="#"><img src="images/template01.png" alt="" class="img-responsive" /></a>
+							</div>
+						</div>
+						<div class="zoom">
+							<div class="image fit">
+								<a href="#"><img src="images/template02.png" alt="" class="img-responsive" /></a>
+							</div>
+						</div>
+						<div class="zoom">
+							<div class="image fit">
+								<a href="#"><img src="images/template03.png" alt="" class="img-responsive" /></a>
+							</div>
+						</div>
+						<div class="zoom">
+							<div class="image fit">
+								<a href="#"><img src="images/template04.png" alt="" class="img-responsive" /></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+					<ul class="icons">
+						<li><a href="https://twitter.com/designtechti" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="https://facebook.com/designtechti" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<li><a href="https://instagram.com/designtechti" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="mailto:mrc_490@hotmail.com" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+					</ul>
+				</div>
+				<div class="copyright">
+					&copy; <a href="index.php" style="text-decoration: none;">Design Tech TI | Web Development</a> 2016 - <?= date("Y"); ?>. All rights reserved.
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+			
+	</body>
+</html>

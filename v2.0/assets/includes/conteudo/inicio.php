f7e8eda33c71fbd7dcda17e651e1bf9ed1cf0bf2
designtechti <mrc_490@hotmail.com>
2020-06-09 20:26:32 -0300

    Alterado a página inicial

--- v2.0/assets/includes/conteudo/inicio.php
+++ v2.0/assets/includes/conteudo/inicio.php
@@ -1,25 +1,108 @@
-	<h1>Serviços</h1>
-				<!-- <iframe width="100%" height="436" src="https://www.youtube.com/embed/OLjnRyzpXFg?list=UUUxBoaZG39OSLA3-mexqSkg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>							
-				</iframe> 
-				-->	
-				<section class="flex">
-					
-					<div class="inicio">
-						<img src="images/services/desenvolvimento-web.png" class="img" alt="Interface gráfica de editor de texto para programação" title="Interface gráfica de editor de texto para programação" />
-						<h2>Desenvolvimento Web</h2>
-						<p class="inicio">Criação de sites com design responsivo, acessível, velozes e simples, sempre levando em conta os objetivos da clientela. Programação Front e Back-End, utilizando as linguagens HTML, CSS, JavaScript, PHP e MySql. </p>
-					</div>
-					<div class="inicio">
-						<img src="images/services/audiovisual.png" class="img" alt="Interface gráfica de editor de imagens" title="Interface gráfica de editor de imagens" />
-						<h2>Edição Audiovisual</h2>
-						<p class="inicio">Criação de intros para canal no YouTube e vídeos no geral, Edição de Imagens (digitais e vetoriais) edição de som, além de ajustes e edições em geral. </p>
-					</div>
-					
-					<div class="inicio">
-						<img src="images/services/suporte-ti.png" class="img" alt="Banner ilustrando os serviços de manutenção de computadores" title="Banner ilustrando os serviços de manutenção de computadores" />
-						<h2>Suporte TI</h2>
-						<p class="inicio">Instalação, troca de peças, formatação, manutenção tanto hardware como software, em sistemas Windows e Linux. </p>
-					</div>
-				</section>
 
-				
+		<!-- One -->
+		
+			<section id="one" class="wrapper style2">
+				<div class="inner">
+					<div class="grid-style">
+
+						<div>
+							<div class="box">
+								<div class="image fit zoom">
+									<img src="images/sublime.jpg" alt="" class="img-responsive" />
+								</div>
+								<div class="content">
+									<header class="align-center">
+										<p>Notícias & curiosidades sobre tecnologia</p>
+										<h2>Sublime Text: O queridinho dos programadores</h2>
+									</header>
+									<p>O que faz um profissional importante é seu conhecimento sobre as diversas áreas em que ele atua. Isso serve para qualquer profissional, do lixeiro ao alto executivo. Mas um outro fator importante a ser considerado são as ferramentas que o profissional utiliza. Já imaginou o quanto sofre um borracheiro sem um macaco? Profissional competente com as ferramentas certas é o primeiro passo para qualquer negócio ter sucesso.</p>
+									<footer class="align-center">
+										<a href="#" class="button alt">Leia Mais</a>
+									</footer>
+								</div>
+							</div>
+						</div>
+
+						<div>
+							<div class="box">
+								<div class="image fit zoom">
+									<img src="images/wireframes.jpg" alt="" class="img-responsive" />
+								</div>
+								<div class="content">
+									<header class="align-center">
+										<p>Notícias & curiosidades sobre tecnologia</p>
+										<h2>UX e acessibilidade ao navegar na web</h2>
+									</header>
+									<p>Saiba como um deficiente visual consegue navegar nas suas redes sociais, sites de notícias, aplicativos e programas de computador e como desenvolvedores de softwares e web designers ajudam a melhorar o acesso a tecnologia. </p>
+									<footer class="align-center">
+										<a href="#" class="button alt">Leia Mais</a>
+									</footer>
+								</div>
+							</div>
+						</div>
+
+						<div>
+							<div class="box">
+								<div class="image fit zoom">
+									<img src="images/iot.jpg" alt="" class="img-responsive" />
+								</div>
+								<div class="content">
+									<header class="align-center">
+										<p>Notícias & curiosidades sobre tecnologia</p>
+										<h2>IOT - O futuro da humanidade</h2>
+									</header>
+									<p>Entenda como a chamada Internet Das Coisas está mudando o conceito de tecnologia e como isto pode facilitar a nossa vida, trazendo mais conforto e modernidade no cotidiano. </p>
+									<footer class="align-center">
+										<a href="#" class="button alt">Leia Mais</a>
+									</footer>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</section>
+		</div>	
+
+		<!-- Two -->		
+			<section id="two" class="wrapper style3">
+				<div class="inner">
+					<header class="align-center">
+						<p>Design & Tecnologia em Um Só Lugar!</p>
+						<h2>Design Tech TI</h2>
+					</header>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three" class="wrapper style2">
+				<div class="inner">
+					<header class="align-center">
+						<p class="special">Alguns de nossos projetos</p>
+						<h2>Portifólio Web Design</h2>
+					</header>
+					<div class="gallery">
+						<div class="zoom">
+							<div class="image fit">
+								<a href="#"><img src="images/template01.png" alt="" class="img-responsive" /></a>
+							</div>
+						</div>
+						<div class="zoom">
+							<div class="image fit">
+								<a href="#"><img src="images/template02.png" alt="" class="img-responsive" /></a>
+							</div>
+						</div>
+						<div class="zoom">
+							<div class="image fit">
+								<a href="#"><img src="images/template03.png" alt="" class="img-responsive" /></a>
+							</div>
+						</div>
+						<div class="zoom">
+							<div class="image fit">
+								<a href="#"><img src="images/template04.png" alt="" class="img-responsive" /></a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
--- v2.0/index.php
+++ v2.0/index.php
@@ -87,113 +87,19 @@
 				</article>
 			</section>
 
-		<!-- One -->
-		
-			<section id="one" class="wrapper style2">
-				<div class="inner">
-					<div class="grid-style">
+		<!-- 
+			<div class="transparente">
+				<?php
 
-						<div>
-							<div class="box">
-								<div class="image fit zoom">
-									<img src="images/sublime.jpg" alt="" class="img-responsive" />
-								</div>
-								<div class="content">
-									<header class="align-center">
-										<p>Notícias & curiosidades sobre tecnologia</p>
-										<h2>Sublime Text: O queridinho dos programadores</h2>
-									</header>
-									<p>O que faz um profissional importante é seu conhecimento sobre as diversas áreas em que ele atua. Isso serve para qualquer profissional, do lixeiro ao alto executivo. Mas um outro fator importante a ser considerado são as ferramentas que o profissional utiliza. Já imaginou o quanto sofre um borracheiro sem um macaco? Profissional competente com as ferramentas certas é o primeiro passo para qualquer negócio ter sucesso.</p>
-									<footer class="align-center">
-										<a href="#" class="button alt">Leia Mais</a>
-									</footer>
-								</div>
-							</div>
-						</div>
-
-						<div>
-							<div class="box">
-								<div class="image fit zoom">
-									<img src="images/wireframes.jpg" alt="" class="img-responsive" />
-								</div>
-								<div class="content">
-									<header class="align-center">
-										<p>Notícias & curiosidades sobre tecnologia</p>
-										<h2>UX e acessibilidade ao navegar na web</h2>
-									</header>
-									<p>Saiba como um deficiente visual consegue navegar nas suas redes sociais, sites de notícias, aplicativos e programas de computador e como desenvolvedores de softwares e web designers ajudam a melhorar o acesso a tecnologia. </p>
-									<footer class="align-center">
-										<a href="#" class="button alt">Leia Mais</a>
-									</footer>
-								</div>
-							</div>
-						</div>
-
-						<div>
-							<div class="box">
-								<div class="image fit zoom">
-									<img src="images/iot.jpg" alt="" class="img-responsive" />
-								</div>
-								<div class="content">
-									<header class="align-center">
-										<p>Notícias & curiosidades sobre tecnologia</p>
-										<h2>IOT - O futuro da humanidade</h2>
-									</header>
-									<p>Entenda como a chamada Internet Das Coisas está mudando o conceito de tecnologia e como isto pode facilitar a nossa vida, trazendo mais conforto e modernidade no cotidiano. </p>
-									<footer class="align-center">
-										<a href="#" class="button alt">Leia Mais</a>
-									</footer>
-								</div>
-							</div>
-						</div>
-
-					</div>
-				</div>
-			</section>
-		</div>	
-
-		<!-- Two -->		
-			<section id="two" class="wrapper style3">
-				<div class="inner">
-					<header class="align-center">
-						<p>Design & Tecnologia em Um Só Lugar!</p>
-						<h2>Design Tech TI</h2>
-					</header>
-				</div>
-			</section>
-
-		<!-- Three -->
-			<section id="three" class="wrapper style2">
-				<div class="inner">
-					<header class="align-center">
-						<p class="special">Alguns de nossos projetos</p>
-						<h2>Portifólio Web Design</h2>
-					</header>
-					<div class="gallery">
-						<div class="zoom">
-							<div class="image fit">
-								<a href="#"><img src="images/template01.png" alt="" class="img-responsive" /></a>
-							</div>
-						</div>
-						<div class="zoom">
-							<div class="image fit">
-								<a href="#"><img src="images/template02.png" alt="" class="img-responsive" /></a>
-							</div>
-						</div>
-						<div class="zoom">
-							<div class="image fit">
-								<a href="#"><img src="images/template03.png" alt="" class="img-responsive" /></a>
-							</div>
-						</div>
-						<div class="zoom">
-							<div class="image fit">
-								<a href="#"><img src="images/template04.png" alt="" class="img-responsive" /></a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-
+					if (!empty($_GET['p']) && !empty($_GET['a'])) {
+						
+						include ('includes/'.$_GET['p'].'/'.$_GET['a'].'.php');
+					}else {
+						include('includes/conteudo/inicio.php');
+					}
+				?>
+			</div>
+		-->	
 
 		<!-- Footer -->
 			<footer id="footer">

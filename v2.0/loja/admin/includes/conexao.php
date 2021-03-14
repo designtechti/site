8dbbf7152b2b57fc3c796ba36ef7081dcea37f3c
Design Tech TI <mrc_490@hotmail.com>
2021-03-13 21:57:25 -0300

    Excluído um arquivo e removida a senha do banco

--- v2.0/loja/admin/includes/conexao.php
+++ v2.0/loja/admin/includes/conexao.php
@@ -1,5 +1,5 @@
 <?php
 	//Comando que conecta no banco de dados da loja virtual
-	$conexao = mysqli_connect('localhost','root','455301789Bst_490','site');
+	$conexao = mysqli_connect('localhost','root','','site');
 	//var_dump($conexao);
 ?>
